e4e65cff18083de59fcb199f619e06f1f4170e9c
Andrew Grigorev <andrew@ei-grad.ru>
2015-02-16 17:14:16 +0300

    pep8 & some whitespaces

--- src/arconfig.py
+++ src/arconfig.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
 import argparse
 import json
 import sys
 import re
 
+
 def build_config(parser, ns):
+
     config = {}
 
     def default_or_value(ns, dest, default):
@@ -40,6 +43,7 @@ def build_config(parser, ns):
 
 
 class GenConfigAction(argparse.Action):
+
     def __init__(self, option_strings, dest, default=False, required=False, help=None):
         super(self.__class__, self).__init__(
             option_strings=option_strings,
@@ -63,7 +67,9 @@ ARGV = set(filter(lambda x: re.match('^[\w\.]+$', x), sys.argv[1:]))
 
 
 def config_loader(cfg, namespace, type_map):
+
     for key, val in cfg.items():
+
         if isinstance(val, dict):
             try:
                 k = (set(val.keys()) & ARGV)
@@ -71,7 +77,7 @@ def config_loader(cfg, namespace, type_map):
                     _key = k.pop()
                     setattr(namespace, key, _key)
                     config_loader(val[_key], namespace, type_map[key][_key])
-            except KeyError as e:
+            except KeyError:
                 pass
         else:
             vtype = type_map.get(key)
@@ -79,7 +85,9 @@ def config_loader(cfg, namespace, type_map):
 
 
 def type_gen(parser, type_map):
+
     for action in parser._actions:
+
         if isinstance(action, (argparse._HelpAction, argparse._VersionAction, GenConfigAction, LoadConfigAction)):
             pass
         elif isinstance(action, (argparse._AppendAction, argparse._AppendConstAction)):
@@ -88,7 +96,7 @@ def type_gen(parser, type_map):
             type_map[action.dest] = bool
         elif isinstance(action, (argparse._SubParsersAction,)):
             for key, parser in action.choices.items():
-                if not action.dest in type_map:
+                if action.dest not in type_map:
                     type_map[action.dest] = {}
                 type_map[action.dest][key] = type_gen(parser, dict())
         else:
@@ -109,22 +117,33 @@ class LoadConfigAction(argparse._StoreAction):
 
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser()
+
     parser.add_argument("--gen-config", action=GenConfigAction)
     parser.add_argument("--config", action=LoadConfigAction)
 
-    parser.add_argument('-s', action='store', dest='simple_value', help='Store a simple value')
-    parser.add_argument('-c', action='store_const', dest='constant_value', const='value-to-store', help='Store a constant value')
-    parser.add_argument('-t', action='store_true', default=False, dest='boolean_switch', help='Set a switch to true')
-    parser.add_argument('-f', action='store_false', default=False, dest='boolean_switch', help='Set a switch to false')
-    parser.add_argument('-a', action='append', dest='collection', default=[], help='Add repeated values to a list')
-    parser.add_argument('-A', action='append_const', dest='const_collection', const='value-1-to-append', default=[], help='Add different values to list')
-    parser.add_argument('-B', action='append_const', dest='const_collection', const='value-2-to-append', help='Add different values to list')
+    parser.add_argument('-s', action='store', dest='simple_value',
+                        help='Store a simple value')
+    parser.add_argument('-c', action='store_const', dest='constant_value', const='value-to-store',
+                        help='Store a constant value')
+    parser.add_argument('-t', action='store_true', default=False, dest='boolean_switch',
+                        help='Set a switch to true')
+    parser.add_argument('-f', action='store_false', default=False, dest='boolean_switch',
+                        help='Set a switch to false')
+    parser.add_argument('-a', action='append', dest='collection', default=[],
+                        help='Add repeated values to a list')
+    parser.add_argument('-A', action='append_const', dest='const_collection', const='value-1-to-append', default=[],
+                        help='Add different values to list')
+    parser.add_argument('-B', action='append_const', dest='const_collection', const='value-2-to-append',
+                        help='Add different values to list')
     parser.add_argument('-z', action='count', dest='test_count')
 
     group = parser.add_argument_group("Test")
-    group.add_argument('-C', action='append_const', dest='const_collection', const='value-1-to-append', default=[], help='Add different values to list')
-    group.add_argument('-D', action='append_const', dest='const_collection', const='value-2-to-append', help='Add different values to list')
+    group.add_argument('-C', action='append_const', dest='const_collection', const='value-1-to-append', default=[],
+                       help='Add different values to list')
+    group.add_argument('-D', action='append_const', dest='const_collection', const='value-2-to-append',
+                       help='Add different values to list')
     group.add_argument('-v', action='count', dest='test_count1')
 
     options = parser.parse_args()
